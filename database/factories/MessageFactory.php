706f8a54565e2d60a74f01f5d3d5c7128a28d8b1
Ferdiyansyah59 <ferdir5599@gmail.com>
2023-12-01 10:20:16 +0700

    first

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'subject' => fake()->sentence(mt_rand(2, 10)),
+            'message' => fake()->paragraph(),
+            'user_id' => mt_rand(2, 4)
+        ];
+    }
+}
